ebdd1f1d9d749a86d3ea98ca4380090ddeda8ee8
Iulian Radu <infinite.flow@gmail.com>
2023-12-04 16:01:53 -0500

    finished basic example

---
+++ Test1_CleanOpenCV/Test1_CleanOpenCV/Test1_CleanOpenCV.cpp
@@ -0,0 +1,56 @@
+//#include "stdafx.h"
+
+#include <opencv2/opencv.hpp>
+
+#include <iostream>
+#include <string>
+#include <fstream>
+
+
+using namespace cv;
+using namespace std;
+
+
+void checkFileExists(String filename) {
+	ifstream infile;
+	infile.open(filename, ios::in);
+	if (infile.fail())
+	{
+		cout << "ERROR. Failed to open file.\n";
+		return;
+	}
+	else {
+		cout << "File opened.\n";
+		infile.close();
+		return;
+	}
+}
+
+
+int main(int argc, char** argv)
+{
+	String filename = "..\\IulianData\\Iulian_Headshot.jpg";
+	checkFileExists(filename);
+
+	// Read the image file
+	Mat image = imread(filename);
+
+	if (image.empty()) // Check for failure
+	{
+		cout << "Could not open or find the image" << endl;
+		system("pause"); //wait for any key press
+		return -1;
+	}
+
+	String windowName = "My HelloWorld Window"; //Name of the window
+
+	namedWindow(windowName); // Create a window
+
+	imshow(windowName, image); // Show our image inside the created window.
+
+	waitKey(0); // Wait for any keystroke in the window
+
+	destroyWindow(windowName); //destroy the created window
+
+	return 0;
+}
